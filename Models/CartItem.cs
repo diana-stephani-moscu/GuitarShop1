90673c75ccb918857c31632b57ba3647f822206b
Diana Moscu <dianamoscu19@gmail.com>
2024-02-17 03:05:47 +0200

    Add project files.

---
+++ Models/CartItem.cs
@@ -0,0 +1,14 @@
+﻿namespace GuitarShop.Models
+{
+    public class CartItem
+    {
+            public int Id { get; set; }
+            public int GuitarId { get; set; }
+            public int Quantity { get; set; }
+            public int UserId { get; set; }
+
+            public Customer Username { get; set; }
+            public Customer Password { get; set; }
+        // Add more properties as needed (e.g., UserId, timestamp, etc.).
+    }
+}
